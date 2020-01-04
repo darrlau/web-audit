ab8b7ebf371caab37735f4bac30877ec82b5d346
Darren Lau <darrlau@gmail.com>
2020-01-03 16:19:56 -0800

    adding authentication

---
+++ src/CurrentUser.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const CurrentUser = ({ displayName, photoURL, email, children }) => {
+  return (
+    <section className="CurrentUser">
+      <div className="CurrentUser--profile">
+        {photoURL && <img src={photoURL} alt={displayName} />}
+        <div className="CurrentUser--information">
+          <h2>{displayName}</h2>
+          <p className="email">{email}</p>
+        </div>
+      </div>
+      <div>
+        <div>{children}</div>
+        <button>Sign Out</button>
+      </div>
+    </section>
+  );
+};
+
+CurrentUser.defaultProps = {
+  displayName: "Bill Murray",
+  email: "billmurray@mailinator.com",
+  photoURL: "https://www.fillmurray.com/300/300",
+  createdAt: new Date()
+};
+
+export default CurrentUser;
